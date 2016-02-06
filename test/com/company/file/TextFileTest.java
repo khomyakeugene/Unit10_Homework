8d2f7422d0801abf16131e48cb41a14fdade43bd
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-02-06 13:19:13 +0200

    Unit 11 Homework

---
+++ test/com/company/file/TextFileTest.java
@@ -0,0 +1,50 @@
+package com.company.file;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by Yevgen on 06.02.2016 as a part of the project "Unit10_Homework".
+ */
+public class TextFileTest {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+
+    }
+
+    @Test
+    public void testGetAbsoluteFileName() throws Exception {
+
+    }
+
+    @Test
+    public void testWriteListToFile() throws Exception {
+
+    }
+
+    @Test
+    public void testWriteEncodedListToFile() throws Exception {
+
+    }
+
+    @Test
+    public void testWriteEncodedUsingDefaultShiftListToFile() throws Exception {
+
+    }
+
+    @Test
+    public void testReadListFromFile() throws Exception {
+
+    }
+
+    @Test
+    public void testReadDecodedListFromFile() throws Exception {
+
+    }
+
+    @Test
+    public void testReadDecodedUsingDefaultShiftListToFile() throws Exception {
+
+    }
+}
