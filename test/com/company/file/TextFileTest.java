dace7e5e839ff9b6c87b3cf52df3349f2e91bde9
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-02-06 15:12:46 +0200

    Unit 11 Homework

--- test/com/company/file/TextFileTest.java
+++ test/com/company/file/TextFileTest.java
@@ -1,40 +1,71 @@
 package com.company.file;
 
+import com.company.caesar.Caesar;
+import com.company.utils.TestUtil;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static com.company.utils.TestUtil.assertArrayListEquals;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Yevgen on 06.02.2016 as a part of the project "Unit10_Homework".
  */
 public class TextFileTest {
+    public final static String CANNOT_READ_FILE_PATTERN = "Cannot open read file \"%s\"!";
+
+    final static int DEFAULT_SHIFT = 30;
+    final static String FILENAME = "test.txt";
+    private static ArrayList<String> text;
 
     @BeforeClass
     public static void setUpClass() throws Exception {
-
+        text = new ArrayList<>();
+        
+        text.add("This is an example");
+        text.add("of a file writing");
+        text.add("and a file reading");
     }
 
     @Test
     public void testGetAbsoluteFileName() throws Exception {
+        final String result = TextFile.getAbsoluteFileName(FILENAME);
 
+        final String expected = new File(FILENAME).getAbsolutePath();
+        assertEquals(expected, result);
     }
 
     @Test
     public void testWriteListToFile() throws Exception {
+        final String fileName = TextFile.writeListToFile(FILENAME, text);
 
+        ArrayList<String> result = TestUtil.readFromFile(fileName);
+        assertArrayListEquals(text, result);
     }
+
 
     @Test
     public void testWriteEncodedListToFile() throws Exception {
+        final String fileName = TextFile.writeEncodedListToFile(FILENAME, text, DEFAULT_SHIFT);
 
+        ArrayList<String> result = TestUtil.readFromFile(fileName);
+        assertArrayListEquals(text, Caesar.decodeList(result, DEFAULT_SHIFT));
     }
 
     @Test
     public void testWriteEncodedUsingDefaultShiftListToFile() throws Exception {
+        final String fileName = TextFile.writeEncodedUsingDefaultShiftListToFile(FILENAME, text);
 
+        ArrayList<String> result = TestUtil.readFromFile(fileName);
+        assertArrayListEquals(text, Caesar.decodeList(result, TextFile.DEFAULT_SHIFT));
     }
 
     @Test
     public void testReadListFromFile() throws Exception {
+        // ArrayList<String> result = TextFile.readListFromFile()
 
     }
 
--- test/com/company/utils/TestUtil.java
+++ test/com/company/utils/TestUtil.java
@@ -1,7 +1,14 @@
 package com.company.utils;
 
 import org.junit.Assert;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by Yevgen on 06.02.2016 as a part of the project "Unit9_Homework".
@@ -9,5 +16,17 @@ import java.util.ArrayList;
 public class TestUtil {
     public static void assertArrayListEquals(ArrayList<? extends Object> expected, ArrayList<? extends Object> actual) {
         Assert.assertArrayEquals(expected.toArray(), actual.toArray());
+    }
+
+    public static ArrayList<String> readFromFile(String fileName) {
+        List<String> list = new ArrayList<>();
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            list = stream
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new ArrayList<>(list);
     }
 }
